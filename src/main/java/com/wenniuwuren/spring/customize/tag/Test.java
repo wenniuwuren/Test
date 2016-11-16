5d431d5a462a512483692f4e86d7d06b8ded0bb9
hzzhuyibin <hzzhuyibin@corp.netease.com>
2016-11-16 19:43:58 +0800

    spring test

--- src/main/java/com/wenniuwuren/spring/customize/tag/Test.java
+++ src/main/java/com/wenniuwuren/spring/customize/tag/Test.java
@@ -1,7 +1,10 @@
 package main.java.com.wenniuwuren.spring.customize.tag;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Created by hzzhuyibin on 2016/11/14.
@@ -9,7 +12,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
     public static void main(String[] args) {
-        ApplicationContext bf = new ClassPathXmlApplicationContext("main/resources/testCustomizeBean.xml");
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("main/resources/testCustomizeBean.xml"));
+        // ApplicationContext bf = new ClassPathXmlApplicationContext("main/resources/testCustomizeBean.xml");
         User user = (User) bf.getBean("testCustomizeBean");
         System.out.println(user.getUserName() + ", " + user.getEmail());
     }
