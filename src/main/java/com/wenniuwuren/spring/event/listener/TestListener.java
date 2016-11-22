3dc317ac546556daba8351488b256166309214b3
hzzhuyibin <hzzhuyibin@corp.netease.com>
2016-11-22 13:46:52 +0800

    add: spring listener test case

--- src/main/java/com/wenniuwuren/spring/event/listener/TestListener.java
+++ src/main/java/com/wenniuwuren/spring/event/listener/TestListener.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
 /**
+ * 定义监听器
  * Created by hzzhuyibin on 2016/11/22.
  */
 public class TestListener implements ApplicationListener{
