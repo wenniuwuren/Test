8da91110bdefb6ebdedcc9545ee0bca58cac9410
hzzhuyibin <hzzhuyibin@corp.netease.com>
2017-03-22 21:12:16 +0800

    add: redis distributed lock

---
+++ src/main/java/com/wenniuwuren/redis/lock/Test.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)Test.java, 2017年03月22日.
+ * Copyright 2017 Netease, Inc. All rights reserved.
+ * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.wenniuwuren.redis.lock;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created by hzzhuyibin on 2017/3/22.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
+
