8ae154a70047b54efbbe9230141a9cef0f43f1a6
hzzhuyibin <hzzhuyibin@corp.netease.com>
2017-04-01 20:38:41 +0800

    change: fork join test case

---
+++ src/main/java/com/wenniuwuren/java/forkjoin/CountForkJoin.java
@@ -0,0 +1,71 @@
+package com.wenniuwuren.java.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * 计算 1+2+3+..+n 的结果
+ * Created by hzzhuyibin on 2017/4/1.
+ */
+public class CountForkJoin extends RecursiveTask<Integer>{
+
+    private static final int THRESHOLD = 2;
+
+    private int start;
+
+    private int end;
+
+    public CountForkJoin(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+
+    @Override
+    protected Integer compute() {
+
+        int sum = 0;
+
+        // 如果任务足够小就直接计算任务
+        boolean canComputeDirectly = (end - start) <= THRESHOLD;
+
+        if (canComputeDirectly) {
+            while (start <= end) {
+                sum += start;
+                start++;
+            }
+        } else {
+            int middle = (start + end) / 2;
+            CountForkJoin leftCount = new CountForkJoin(start, middle);
+            CountForkJoin rightCount = new CountForkJoin(middle + 1, end);
+
+            // 拆分执行子任务
+            leftCount.fork();
+            rightCount.fork();
+
+            int leftResult = leftCount.join();
+            int rightResult = rightCount.join();
+
+            // 合并结果
+            sum = leftResult + rightResult;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+            CountForkJoin countForkJoin = new CountForkJoin(1, 6);
+
+            Future<Integer> result = forkJoinPool.submit(countForkJoin);
+
+            System.out.println(result.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
