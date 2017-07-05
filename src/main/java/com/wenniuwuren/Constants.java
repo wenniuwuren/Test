bb2d0816500261302f2c50095e2b4ea6c8ab943d
hzzhuyibin <hzzhuyibin@corp.netease.com>
2017-07-05 21:01:28 +0800

    change: redis in action chapter6

--- src/main/java/com/wenniuwuren/Constants.java
+++ src/main/java/com/wenniuwuren/Constants.java
@@ -6,4 +6,5 @@ package com.wenniuwuren;
 public class Constants {
 
     public static final String ZK_HOST = "192.168.243.128:2181";
+    public static final String REDIS_HOST_COMPANY = "192.168.227.128";
 }
