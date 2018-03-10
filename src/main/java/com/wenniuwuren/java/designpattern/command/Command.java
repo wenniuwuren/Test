fb1a548e723419cb3ae573d6d9f1756fae4fae37
hzzhuyibin <hzzhuyibin@corp.netease.com>
2018-03-10 21:59:38 +0800

    add：命令模式

---
+++ src/main/java/com/wenniuwuren/java/designpattern/command/Command.java
@@ -0,0 +1,8 @@
+package com.wenniuwuren.java.designpattern.command;
+
+public interface Command {
+    /**
+     * 执行方法
+     */
+    void execute();
+}
---
+++ src/main/java/com/wenniuwuren/java/designpattern/command/Receiver.java
@@ -0,0 +1,10 @@
+package com.wenniuwuren.java.designpattern.command;
+
+public class Receiver {
+    /**
+     * 真正执行命令相应的操作
+     */
+    public void action() {
+        System.out.println("执行操作");
+    }
+}
