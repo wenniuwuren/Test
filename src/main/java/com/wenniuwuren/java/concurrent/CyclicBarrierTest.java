dca349f740cc0e791ea63ce4e94dc31dc3159bda
hzzhuyibin <hzzhuyibin@corp.netease.com>
2018-03-20 20:22:05 +0800

    add：CyclicBarrier Test

---
+++ src/main/java/com/wenniuwuren/java/concurrent/CyclicBarrierTest.java
@@ -0,0 +1,38 @@
+package com.wenniuwuren.java.concurrent;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CyclicBarrierTest {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        final CyclicBarrier cb = new CyclicBarrier(3);//创建CyclicBarrier对象并设置3个公共屏障点
+        for (int i = 0; i < 3; i++) {
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName() +
+                                "即将到达集合地点1，当前已有" + cb.getNumberWaiting() + "个已经到达，正在等候");
+                        cb.await();//到此如果没有达到公共屏障点，则该线程处于等待状态，如果达到公共屏障点则所有处于等待的线程都继续往下运行  
+
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName() +
+                                "即将到达集合地点2，当前已有" + cb.getNumberWaiting() + "个已经到达，正在等候");
+                        cb.await(); // 3个线程都到达后，就可以循环使用了
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("线程" + Thread.currentThread().getName() +
+                                "即将到达集合地点3，当前已有" + cb.getNumberWaiting() + "个已经到达，正在等候");
+                        cb.await();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.execute(runnable);
+        }
+        service.shutdown();
+    }
+}  
