a475a48b03c6be9abab695c5a745a0f5355ec533
hzzhuyibin <hzzhuyibin@corp.netease.com>
2020-10-04 22:55:00 +0800

    change: algorithm

--- src/main/java/com/wenniuwuren/algorithm/lru/linkedlist/AddTwoNumbers.java
+++ src/main/java/com/wenniuwuren/algorithm/lru/linkedlist/AddTwoNumbers.java
@@ -9,80 +9,79 @@ import java.util.Deque;
 
 /**
  * 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
- * 输出：7 -> 8 -> 0 -> 7
+ * 输出：2 -> 9 -> 8 -> 3
  */
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Deque<ListNode> stack1 = new ArrayDeque<>();
-        Deque<ListNode> stack2 = new ArrayDeque<>();
+        ListNode result = new ListNode(-1);
+        ListNode r = result;
 
-        while (l1 != null) {
-            stack1.push(l1);
+        int add = 0; // 进位
+        while (l1 != null && l2!= null) {
+            int currNum = (l1.val + l2.val + add)%10;
+            add = (l1.val + l2.val + add)/10;
+
+            result.next = new ListNode(currNum);
+
+            // next
+            result = result.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 == null && l2 != null) {
+            int currNum = (l2.val + add)%10;
+            add = (l2.val + add)/10;
+
+            result.next = new ListNode(currNum);
+
+            result = result.next;
+            l2 = l2.next;
+        }
+
+        while (l2 == null && l1 != null) {
+            int currNum = (l1.val + add)%10;
+            add = (l1.val + add)/10;
+
+            result.next = new ListNode(currNum);
+
+            result = result.next;
             l1 = l1.next;
         }
 
-        while (l2 != null) {
-            stack2.push(l2);
-            l2 = l2.next;
-        }
-        ListNode nextNode = null;
-        // 是否需要进一位
-        boolean isEnterOne = false;
-        while (!stack1.isEmpty() || !stack2.isEmpty() || isEnterOne) {
-            int sum = 0;
-
-            if (isEnterOne) {
-                sum = sum + 1;
-                isEnterOne = false;
-            }
-
-            if (stack1.isEmpty() && !stack2.isEmpty()) {
-                sum += stack2.pop().val;
-            }
-
-            if (!stack1.isEmpty() && stack2.isEmpty()) {
-                sum += stack1.pop().val;
-            }
-
-            if (!stack1.isEmpty() && !stack2.isEmpty()) {
-                sum += stack1.pop().val + stack2.pop().val;
-            }
-
-
-            if (sum >= 10) {
-                isEnterOne = true;
-                sum = sum - 10;
-            }
-
-            if (nextNode == null) {
-                nextNode = new ListNode(sum);
-            } else {
-                ListNode currentNode = new ListNode(sum);
-                currentNode.next = nextNode;
-                nextNode = currentNode;
-            }
-
+        if (add != 0) { // 最后的进位只会加一次
+            result.next = new ListNode(add);
         }
 
-        return nextNode;
 
+        return r.next;
     }
 
     public static void main(String[] args) {
-        /*ListNode listNode1 = new ListNode(7);
-        ListNode listNode2 = new ListNode(2);
-        ListNode listNode3 = new ListNode(4);
-        ListNode listNode4 = new ListNode(3);
+       /* ListNode listNode1 = new ListNode(9);
+        ListNode listNode2 = new ListNode(9);
+        ListNode listNode3 = new ListNode(9);
+        ListNode listNode4 = new ListNode(9);
+        ListNode listNode8 = new ListNode(9);
+        ListNode listNode9 = new ListNode(9);
+        ListNode listNode10 = new ListNode(9);
+
+
         listNode1.next = listNode2;
         listNode2.next = listNode3;
         listNode3.next = listNode4;
+        listNode4.next = listNode8;
+        listNode8.next = listNode9;
+        listNode9.next = listNode10;
 
-        ListNode listNode5 = new ListNode(5);
-        ListNode listNode6 = new ListNode(6);
-        ListNode listNode7 = new ListNode(4);
+        ListNode listNode5 = new ListNode(9);
+        ListNode listNode6 = new ListNode(9);
+        ListNode listNode7 = new ListNode(9);
+        ListNode listNode11 = new ListNode(9);
         listNode5.next = listNode6;
-        listNode6.next = listNode7;*/
+        listNode6.next = listNode7;
+        listNode7.next = listNode11;*/
 
         ListNode listNode1 = new ListNode(5);
         ListNode listNode5 = new ListNode(5);
